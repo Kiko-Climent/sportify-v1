df4ae570c0fb6763fe90a09447bb0a38dbd3b125
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2024-11-27 20:00:16 +0100

    fixing proxy

--- api/proxy.js
+++ api/proxy.js
@@ -1,20 +1,40 @@
 export default async function handler(req, res) {
-  const { query, location, radius } = req.query;
+  const {query, location, radius} = req.query;
+  console.log("Proxy Query Params:", { query, location, radius });
 
-  // Dividir la cadena de ubicación en latitud y longitud
-  const [latitude, longitude] = location.split(',');
 
-  console.log("Proxy Query Params:", { query, latitude, longitude, radius });
-
-  const apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&location=${latitude},${longitude}&radius=${radius}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
+  const apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&location=${location.latitude},${location.longitude}&radius=${radius}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
+  console.log("Generated Google API URL:", apiUrl);
 
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
 
     res.status(200).json(data);
-  } catch (error) {
+  } catch(error) {
     console.error("Error fetching Google Places:", error);
     res.status(500).json({ error: "Failed to fetch data from Google Places" });
   }
 }
+
+
+// export default async function handler(req, res) {
+//   const { query, location, radius } = req.query;
+
+//   // Dividir la cadena de ubicación en latitud y longitud
+//   const [latitude, longitude] = location.split(',');
+
+//   console.log("Proxy Query Params:", { query, latitude, longitude, radius });
+
+//   const apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&location=${latitude},${longitude}&radius=${radius}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
+//   console.log("Generated Google API URL:", apiUrl);
+//   try {
+//     const response = await fetch(apiUrl);
+//     const data = await response.json();
+
+//     res.status(200).json(data);
+//   } catch (error) {
+//     console.error("Error fetching Google Places:", error);
+//     res.status(500).json({ error: "Failed to fetch data from Google Places" });
+//   }
+// }
--- src/features/googlePlaces.js
+++ src/features/googlePlaces.js
@@ -1,3 +1,36 @@
+// import {
+//   fetchGoogleLocationsStart,
+//   fetchGoogleLocationsSuccess,
+//   fetchGoogleLocationsFailure,
+// } from "../redux/slices/sportsGoogleSlice";
+
+// export const fetchGooglePlaces = (query, location, radius = 5000) => async (dispatch) => {
+//   // const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&location=${location.latitude},${location.longitude}&radius=${radius}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
+//   const proxyUrl = process.env.REACT_APP_PROXY_URL;
+//   const url = `${proxyUrl}?query=${query}&location=${location.latitude},${location.longitude}&radius=${radius}`;
+//   console.log('Calling API URL:', url);
+
+//   dispatch(fetchGoogleLocationsStart());
+
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+    
+
+//     if (data.status === "OK") {
+//       console.log("Google Places Results:", data.results);
+//       dispatch(fetchGoogleLocationsSuccess(data.results));
+//     } else {
+//       console.error("Error fetching Google Places:", data.status);
+//       dispatch(fetchGoogleLocationsFailure(data.status));
+//     }
+//   } catch (error) {
+//     console.error("Error fetching Google Places:", error.message);
+//     dispatch(fetchGoogleLocationsFailure(error.message));
+//   }
+// };
+
+
 import {
   fetchGoogleLocationsStart,
   fetchGoogleLocationsSuccess,
@@ -5,9 +38,19 @@ import {
 } from "../redux/slices/sportsGoogleSlice";
 
 export const fetchGooglePlaces = (query, location, radius = 5000) => async (dispatch) => {
-  // const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&location=${location.latitude},${location.longitude}&radius=${radius}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
+  // Si location es una cadena, la dividimos en latitud y longitud
+  let latitude, longitude;
+  if (typeof location === 'string') {
+    [latitude, longitude] = location.split(',');
+  } else {
+    // Si location ya es un objeto, usamos las propiedades directamente
+    latitude = location.latitude;
+    longitude = location.longitude;
+  }
+
+  // Construcción de la URL
   const proxyUrl = process.env.REACT_APP_PROXY_URL;
-  const url = `${proxyUrl}?query=${query}&location=${location.latitude},${location.longitude}&radius=${radius}`;
+  const url = `${proxyUrl}?query=${query}&location=${latitude},${longitude}&radius=${radius}`;
   console.log('Calling API URL:', url);
 
   dispatch(fetchGoogleLocationsStart());
@@ -16,7 +59,6 @@ export const fetchGooglePlaces = (query, location, radius = 5000) => async (disp
     const response = await fetch(url);
     const data = await response.json();
     
-
     if (data.status === "OK") {
       console.log("Google Places Results:", data.results);
       dispatch(fetchGoogleLocationsSuccess(data.results));
