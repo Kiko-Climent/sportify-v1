9e035d4d8cee8018f878c4fed540618602db8001
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2024-12-03 23:33:16 +0100

    Deploy2

--- api/proxy.js
+++ api/proxy.js
@@ -1,18 +1,25 @@
+// api/proxy.js
 export default async function handler(req, res) {
-  const {query, location, radius} = req.query;
-  console.log("Proxy Query Params:", { query, location, radius });
+  const { query, latitude, longitude, radius } = req.query;
 
+  if (!query || !latitude || !longitude) {
+    return res.status(400).json({ error: "Missing required parameters" });
+  }
 
-  const apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&location=${location.latitude},${location.longitude}&radius=${radius}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
-  console.log("Generated Google API URL:", apiUrl);
+  const apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&location=${latitude},${longitude}&radius=${radius || 5000}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
 
   try {
+    
     const response = await fetch(apiUrl);
     const data = await response.json();
 
-    res.status(200).json(data);
-  } catch(error) {
-    console.error("Error fetching Google Places:", error);
-    res.status(500).json({ error: "Failed to fetch data from Google Places" });
+    if (data.status === "OK") {
+      res.status(200).json(data.results);
+    } else {
+      res.status(400).json({ error: data.status });
+    }
+  } catch (error) {
+    console.error("Error fetching Google Places:", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 }
