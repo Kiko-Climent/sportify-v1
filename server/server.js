5e4df6d75b36597559743e2f25b1fb9d4bafe2da
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2024-12-11 18:10:43 +0100

    Change google api key

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+  origin: 'http://localhost:3000', // Permitir solicitudes solo desde este origen
+}));
+
 // Middleware
 app.use(cors());
 app.use(express.json());
